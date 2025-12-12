ed8ca53fa4f52ed44a98eb96077fac71d3b25e04
emmanule <emmanuelokwara001@gmail.com>
2025-12-12 09:55:50 +0200

    changes made

--- apps/web/lib/components/property/components/property-list.tsx
+++ apps/web/lib/components/property/components/property-list.tsx
@@ -67,6 +67,7 @@ const PropertyDetailDialog = ({ children }: { children: React.ReactNode }) => {
   const { data: c } = useProperty();
   const { setData } = usePropertyFormContext();
 
+
   useEffect(() => {
     setData(c as CreatePropertyDto);
     return () => {};
@@ -77,7 +78,7 @@ const PropertyDetailDialog = ({ children }: { children: React.ReactNode }) => {
       <DialogTrigger asChild>
         <div>{children}</div>
       </DialogTrigger>
-      <DialogContent className=" flex justify-center w-[60%] h-[70%] items-center bg-gradient-to-r from-l_f_f  to-l_f_s  data-[state=open]:border-[0.5px] data-[state=open]:border-button">
+      <DialogContent  className=" flex justify-center w-[50%] h-[70%] items-center bg-gradient-to-r from-l_f_f  to-l_f_s  data-[state=open]:border-[0.5px] data-[state=open]:border-button">
         <DialogTitle></DialogTitle>
         <Flex
           iscol
